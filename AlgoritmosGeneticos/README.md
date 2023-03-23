1252d2eb3309dae311fc7db2d905bc110b079fb0
Raphaella Tamyres Siqueira Gonçalves <raphaella220046@ilum.cnpem.br>
2023-03-23 17:41:16 -0300

    Inclusão do experimento A.04 - caixas nao-binarias e pseudocódigo do experimento A.05 na pasta AlgoritmosGeneticos, e atualização do README.md desta pasta

--- AlgoritmosGeneticos/README.md
+++ AlgoritmosGeneticos/README.md
@@ -14,8 +14,10 @@ Esceva um README aqui para guiar o leitor sobre seus experimentos realizados.
 ### Experimentos:
 
 
-**O Experimento A.01 - busca aleatoria** foi realizado com o intuito de solucionar o problema de otimização com caixas binárias. A solução apresentada é probabilística e o resultado obtido varia em cada execução do algoritmo, podendo ou não ser encontrada a função objetivo.
+**O Experimento A.01 - busca aleatoria** consiste em um problema de maximização, cujo fit é o valor máximo de caixas. Foi realizado com o intuito de solucionar o problema de otimização com caixas binárias. A solução apresentada é probabilística e o resultado obtido varia em cada execução do algoritmo, podendo ou não ser encontrada a função objetivo.
 
-**O Experimento A.02 - busca em grade** foi realizado com o intuito de testar todas as possibilidades de um conjunto binário. A solução apresentada é determinística e o resultado obtido é constante em todas as vezes que o algoritmo é executado. Desta maneira, a função objetiva sempre estará entre as combinações apresentadas.
+**O Experimento A.02 - busca em grade** Ao contrário do experimento anterior, foi realizado com o intuito de testar todas as possibilidades de um conjunto binário. A solução apresentada é determinística e o resultado obtido é constante em todas as vezes que o algoritmo é executado. Desta maneira, a função objetivo sempre estará entre as combinações apresentadas.
 
-**O Experimento A.03 - algoritmo genetico** é probabilístico e cria uma população aleatória para, em seguida, gerar um cruzamento aleatório entre os indivíduos, por meio da combinação de genes dos pais e das mães. Por último, é utilizada uma função para fazer uma mutação aleatória nos genes desse indivíduo.
+**O Experimento A.03 - algoritmo genetico** é um algoritmo probabilístico e cria uma população aleatória para, em seguida, gerar um cruzamento aleatório entre os indivíduos, por meio da combinação de genes dos pais e das mães. Por último, é utilizada uma função para fazer uma mutação aleatória nos genes desse indivíduo.
+
+**O Experimento A.04 - caixas nao-binarias** é um algoritmo que gera uma população de indivíduos, cujos genes não são binários, mas compreende um intervalo de 0 a 100 (incluso). Sendo assim, as funções utilizadas no experimento anterior foram alteradas considerando a variável de valor máximo em cada caixa.Foi constatado que dada a alta probabilidade de variabilidade genética entre os indivíduos em cruzamento, além das mutações ocorrerem ao acaso, é bastante improvável que o algoritmo apresente o fitness esperado.
--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -4,11 +4,98 @@ def gene_cb():
     '''Gera um gene válido para o problema das caixas binárias.
     
     Return:
-        Um valor 0 ou 1'''
+        Um valor 0 ou 1
+    '''
     
     lista = [0, 1]
     gene = random.choice(lista)
     return gene
+
+def gene_cnb(valor_max_caixa):
+    '''Gera um gene válido para o problema das caixas não -binárias
+    
+    Args:
+        valor_max_caixa: valor máximo que a caixa pode assumir
+        
+    Returns:
+        Um valor de 0 a 'valor_max_caixa' (incluso)
+    '''
+    gene = random.randint (0, valor_max_caixa)
+    return gene
+
+def individuo_cnb(numero_genes, valor_max_caixa):
+    '''Gera um indivíduo válido para o problema das caixas não binárias
+    
+    Args:
+        numero_genes: número de genes na lista que representa o indivíduo
+        valor_max_caixa: valor máximo que a caixa pode assumir
+    
+    Returns:
+        Uma lista que representa um indivíduo válido para o problema das CNB
+    '''
+    individuo = []
+    for _ in range(numero_genes):
+        gene = gene_cnb(valor_max_caixa)
+        individuo.append(gene)
+    return individuo
+
+def populacao_cnb(tamanho_populacao, numero_genes, valor_max_caixa):
+    '''Cria uma população de indivíduos para o problema das caixas não-binárias
+    
+    Args:
+        tamanho_populacao: número de indivíduos da população
+        numero_genes: número de genes do indivíduo
+        valor_max_caixa: valor máximo que a caixa pode assumir
+        
+    Returns:
+        uma lista onde cada item representa um indivíduo
+    '''
+    populacao = []
+    for _ in range(tamanho_populacao):
+        individuo = individuo_cnb(numero_genes, valor_max_caixa)
+        populacao.append(individuo)
+    return populacao
+
+def funcao_objetivo_cnb(individuo):
+    '''Calcula o fitness do indivíduo para o problema das caixas não-binárias
+    
+    Args:
+        individuo: lista que representa um indivíduo dentro do problema das CNB
+        
+    Returns:
+        Um valor que representa o fitness do indivíduo
+    '''
+    fitness = sum(individuo)
+    return fitness
+
+def funcao_objetivo_pop_cnb(populacao):
+    '''Calcula o fitness da população completa
+    
+    Args:
+        populacao: lista com todos os indivíduos da população
+        
+    Returns:
+        Uma lista com o fitness de cada indivíduo em ordem
+    '''
+    fitness_pop = []
+    for individuo in populacao:
+        fitness_ind = funcao_objetivo_cnb(individuo)
+        fitness_pop.append(fitness_ind)
+    return fitness_pop
+
+def mutacao_cnb(individuo, valor_max_caixa):
+    '''Realiza a mutação do indivíduo
+    
+    Args:
+        individuo: indivíduo que deve sofrer a mutação
+        valor_max_caixa: valor máximo que a caixa pode assumir
+        
+    Returns:
+        Indivíduo que sofreu a mutação
+    '''
+    gene_a_ser_mutado = random.randint(0, len(individuo)-1)
+    individuo[gene_a_ser_mutado] = gene_cnb(valor_max_caixa)
+    return individuo
 
 def individuo_cb(n):
     ''' Gera um indivíduo para o problema das caixas binárias.
@@ -17,7 +104,8 @@ def individuo_cb(n):
         n = número de genes do indivíduo
         
     Return:
-        Uma lista com n genes. Cada gene é um valor 0 ou 1.'''
+        Uma lista com n genes. Cada gene é um valor 0 ou 1.
+    '''
     
     individuo = []
     
@@ -34,7 +122,8 @@ def populacao_cb(tamanho, n):
         n = número de indivíduos
         
     Return:
-        Uma lista onde cada item é um indivíduo. Um indivíduo é uma lista com n genes.'''
+        Uma lista onde cada item é um indivíduo. Um indivíduo é uma lista com n genes.
+    '''
     
     populacao = []
     
@@ -52,7 +141,8 @@ def selecao_roleta_max(populacao, fitness):
         fitness = valor da função objetivo dos indivíduos da população
         
     Return:
-        População dos indivíduos selecionados.'''
+        População dos indivíduos selecionados.
+    '''
     
     populacao_selecionada = random.choices(populacao, weights=fitness, k=len(populacao))
     return populacao_selecionada
@@ -65,7 +155,8 @@ def cruzamento_ponto_simples(pai, mae):
         mãe: uma lista representando um indivíduo
     
     Return:
-        Duas listas sendo que cada uma representa um filho dos pais que foram os argumentos.'''
+        Duas listas sendo que cada uma representa um filho dos pais que foram os argumentos.
+    '''
     
     ponto_de_corte = random.randint(1, len(pai) - 1)
     
@@ -81,7 +172,8 @@ def mutacao_cb(individuo):
         individuo: uma lista representando um individuo no problema das caixas binárias.
     
     Return:
-        Um individuo com um gene mutado.'''
+        Um individuo com um gene mutado.
+    '''
     
     gene_a_ser_mutado = random.randint(0, len(individuo) - 1)
     individuo[gene_a_ser_mutado] = gene_cb()
@@ -95,18 +187,20 @@ def funcao_objetivo_cb(individuo):
         individuo = lista contendo os genes das caixas binárias
         
     Return:
-        Um valor representando a soma dos genes do indivíduo.'''
+        Um valor representando a soma dos genes do indivíduo.
+    '''
     
     return sum(individuo) + 1 # Para evitar dar erro no fit em uma seleção de indivíduos com apenas genes 0
 
-def funcao_objetivo_pop_cd(populacao):
+def funcao_objetivo_pop_cb(populacao):
     '''Calcula a função objetivo para todos os membros de uma população.
     
     Args:
         populacao = lista com todos os indivíduos da população
     
     Return:
-        Lista dos valores representando a fitness de cada indivíduo da população.'''
+        Lista dos valores representando a fitness de cada indivíduo da população.
+    '''
         
     fitness = []
     
