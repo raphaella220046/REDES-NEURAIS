637d5b3da864ff6c38e43147626c1bd33430a13c
Raphaella Tamyres Siqueira Gonçalves <raphaella220046@ilum.cnpem.br>
2023-03-30 18:51:08 -0300

    Inclusão do experimento A.05 - descobrindo a senha e atualização do README.md

--- AlgoritmosGeneticos/README.md
+++ AlgoritmosGeneticos/README.md
@@ -14,10 +14,22 @@ Esceva um README aqui para guiar o leitor sobre seus experimentos realizados.
 ### Experimentos:
 
 
-**O Experimento A.01 - busca aleatoria** consiste em um problema de maximização, cujo fit é o valor máximo de caixas. Foi realizado com o intuito de solucionar o problema de otimização com caixas binárias. A solução apresentada é probabilística e o resultado obtido varia em cada execução do algoritmo, podendo ou não ser encontrada a função objetivo.
+**O Experimento A.01 - busca aleatoria**
 
-**O Experimento A.02 - busca em grade** Ao contrário do experimento anterior, foi realizado com o intuito de testar todas as possibilidades de um conjunto binário. A solução apresentada é determinística e o resultado obtido é constante em todas as vezes que o algoritmo é executado. Desta maneira, a função objetivo sempre estará entre as combinações apresentadas.
+Consiste em um problema de maximização, cujo fit é o valor máximo de caixas. Foi realizado com o intuito de solucionar o problema de otimização com caixas binárias. A solução apresentada é probabilística e o resultado obtido varia em cada execução do algoritmo, podendo ou não ser encontrada a função objetivo.
 
-**O Experimento A.03 - algoritmo genetico** é um algoritmo probabilístico e cria uma população aleatória para, em seguida, gerar um cruzamento aleatório entre os indivíduos, por meio da combinação de genes dos pais e das mães. Por último, é utilizada uma função para fazer uma mutação aleatória nos genes desse indivíduo.
+**O Experimento A.02 - busca em grade**
 
-**O Experimento A.04 - caixas nao-binarias** é um algoritmo que gera uma população de indivíduos, cujos genes não são binários, mas compreende um intervalo de 0 a 100 (incluso). Sendo assim, as funções utilizadas no experimento anterior foram alteradas considerando a variável de valor máximo em cada caixa.Foi constatado que dada a alta probabilidade de variabilidade genética entre os indivíduos em cruzamento, além das mutações ocorrerem ao acaso, é bastante improvável que o algoritmo apresente o fitness esperado.
+Ao contrário do experimento anterior, foi realizado com o intuito de testar todas as possibilidades de um conjunto binário. A solução apresentada é determinística e o resultado obtido é constante em todas as vezes que o algoritmo é executado. Desta maneira, a função objetivo sempre estará entre as combinações apresentadas.
+
+**O Experimento A.03 - algoritmo genetico**
+
+É um algoritmo probabilístico e cria uma população aleatória para, em seguida, gerar um cruzamento aleatório entre os indivíduos, por meio da combinação de genes dos pais e das mães. Por último, é utilizada uma função para fazer uma mutação aleatória nos genes desse indivíduo.
+
+**O Experimento A.04 - caixas nao-binarias**
+
+É um algoritmo que gera uma população de indivíduos, cujos genes não são binários, mas compreende um intervalo de 0 a 100 (incluso). Sendo assim, as funções utilizadas no experimento anterior foram alteradas considerando a variável de valor máximo em cada caixa.Foi constatado que dada a alta probabilidade de variabilidade genética entre os indivíduos em cruzamento, além das mutações ocorrerem ao acaso, é bastante improvável que o algoritmo apresente o fitness esperado.
+
+**experimento A.05 - descobrindo a senha**
+
+Este é um algoritmo para resolver um problema de minimização que consiste em descobrir a senha determinada pela distância que as senhas estimadas estão em relação à essa senha determinada, elegendo a senha com melhor fit. Ele funciona a partir de uma lista com a senha de caracteres diversos criada, cada caracter é convertido para um número. Em seguida, são geradas senhas aleatórias com a mesma quantidade de caracteres da senha criada. É aplicada a seleção por torneio, onde as senhas combatem entre si e o melhor fit é definido ao comparar as distâncias entre a senha criada e as melhores senhas geradas. Todo o processo tem convergência para a diferença zero, quando a senha é encontrada.
--- AlgoritmosGeneticos/funcoes.py
+++ AlgoritmosGeneticos/funcoes.py
@@ -20,82 +20,22 @@ def gene_cnb(valor_max_caixa):
     Returns:
         Um valor de 0 a 'valor_max_caixa' (incluso)
     '''
+    
     gene = random.randint (0, valor_max_caixa)
     return gene
 
-def individuo_cnb(numero_genes, valor_max_caixa):
-    '''Gera um indivíduo válido para o problema das caixas não binárias
+def gene_caracter(caracteres):
+    '''Sorteia um caracter
     
     Args:
-        numero_genes: número de genes na lista que representa o indivíduo
-        valor_max_caixa: valor máximo que a caixa pode assumir
-    
-    Returns:
-        Uma lista que representa um indivíduo válido para o problema das CNB
-    '''
-    individuo = []
-    for _ in range(numero_genes):
-        gene = gene_cnb(valor_max_caixa)
-        individuo.append(gene)
-    return individuo
-
-def populacao_cnb(tamanho_populacao, numero_genes, valor_max_caixa):
-    '''Cria uma população de indivíduos para o problema das caixas não-binárias
-    
-    Args:
-        tamanho_populacao: número de indivíduos da população
-        numero_genes: número de genes do indivíduo
-        valor_max_caixa: valor máximo que a caixa pode assumir
+        caracteres: caracteres possíveis de serem sorteados
         
     Returns:
-        uma lista onde cada item representa um indivíduo
+        Retorna um caracter dentro dos possíveis de serem sorteados
     '''
-    populacao = []
-    for _ in range(tamanho_populacao):
-        individuo = individuo_cnb(numero_genes, valor_max_caixa)
-        populacao.append(individuo)
-    return populacao
-
-def funcao_objetivo_cnb(individuo):
-    '''Calcula o fitness do indivíduo para o problema das caixas não-binárias
     
-    Args:
-        individuo: lista que representa um indivíduo dentro do problema das CNB
-        
-    Returns:
-        Um valor que representa o fitness do indivíduo
-    '''
-    fitness = sum(individuo)
-    return fitness
-
-def funcao_objetivo_pop_cnb(populacao):
-    '''Calcula o fitness da população completa
-    
-    Args:
-        populacao: lista com todos os indivíduos da população
-        
-    Returns:
-        Uma lista com o fitness de cada indivíduo em ordem
-    '''
-    fitness_pop = []
-    for individuo in populacao:
-        fitness_ind = funcao_objetivo_cnb(individuo)
-        fitness_pop.append(fitness_ind)
-    return fitness_pop
-
-def mutacao_cnb(individuo, valor_max_caixa):
-    '''Realiza a mutação do indivíduo
-    
-    Args:
-        individuo: indivíduo que deve sofrer a mutação
-        valor_max_caixa: valor máximo que a caixa pode assumir
-        
-    Returns:
-        Indivíduo que sofreu a mutação
-    '''
-    gene_a_ser_mutado = random.randint(0, len(individuo)-1)
-    individuo[gene_a_ser_mutado] = gene_cnb(valor_max_caixa)
-    return individuo
+    caracter = random.choice(caracteres)
+    return caracter
 
 def individuo_cb(n):
     ''' Gera um indivíduo para o problema das caixas binárias.
@@ -114,6 +54,41 @@ def individuo_cb(n):
         individuo.append(gene)
     return individuo
 
+def individuo_cnb(numero_genes, valor_max_caixa):
+    '''Gera um indivíduo válido para o problema das caixas não binárias
+    
+    Args:
+        numero_genes: número de genes na lista que representa o indivíduo
+        valor_max_caixa: valor máximo que a caixa pode assumir
+    
+    Returns:
+        Uma lista que representa um indivíduo válido para o problema das CNB
+    '''
+    
+    individuo = []
+    for _ in range(numero_genes):
+        gene = gene_cnb(valor_max_caixa)
+        individuo.append(gene)
+    return individuo
+
+def individuo_senha(tamanho_senha, caracteres):
+    '''Cria um candidato para o problema da senha
+    
+    Args:
+        tamanho_senha: número inteiro representando o tamanho da senha
+        caracteres: caracteres possíveis a serem sorteados
+        
+    Returns:
+        Lista com n letras
+    '''
+    
+    candidato = []
+    
+    for n in range(tamanho_senha):
+        candidato.append(gene_caracter(caracteres))
+    
+    return candidato
+
 def populacao_cb(tamanho, n):
     '''Cria uma população no problema das caixas binárias.
     
@@ -130,55 +105,41 @@ def populacao_cb(tamanho, n):
     for _ in range(tamanho): # o uso do _ é para uma variável que não será usada no algoritmo
         populacao.append(individuo_cb(n))
     return populacao
+
+def populacao_cnb(tamanho_populacao, numero_genes, valor_max_caixa):
+    '''Cria uma população de indivíduos para o problema das caixas não-binárias
     
-def selecao_roleta_max(populacao, fitness):
-    '''Seleciona individuos de uma população usando o método da roleta.
-    
-    Nota: apenas funciona para problemas de maximização.
-    
-    Arg:
-        população = lista com todos os indivíduos da população
-        fitness = valor da função objetivo dos indivíduos da população
+    Args:
+        tamanho_populacao: número de indivíduos da população
+        numero_genes: número de genes do indivíduo
+        valor_max_caixa: valor máximo que a caixa pode assumir
         
-    Return:
-        População dos indivíduos selecionados.
+    Returns:
+        uma lista onde cada item representa um indivíduo
     '''
     
-    populacao_selecionada = random.choices(populacao, weights=fitness, k=len(populacao))
-    return populacao_selecionada
+    populacao = []
+    for _ in range(tamanho_populacao):
+        individuo = individuo_cnb(numero_genes, valor_max_caixa)
+        populacao.append(individuo)
+    return populacao
 
-def cruzamento_ponto_simples(pai, mae):
-    '''Operador de cruzamento de ponto simples.
+def populacao_inicial_senha(tamanho, tamanho_senha, caracteres):
+    '''Cria população inicial no problema da senha
     
     Args:
-        pai: uma lista representando um indivíduo
-        mãe: uma lista representando um indivíduo
-    
-    Return:
-        Duas listas sendo que cada uma representa um filho dos pais que foram os argumentos.
+        tamanho: tamanho da população
+        tamanho_senha: número inteiro que representa o tamanho da senha
+        caracteres: caracteres possíveis de serem sorteados
+        
+        Returns:
+            Lista com todos os indivíduos da população no problema da senha
     '''
     
-    ponto_de_corte = random.randint(1, len(pai) - 1)
-    
-    filho1 = pai[:ponto_de_corte] + mae[ponto_de_corte:]
-    filho2 = mae[:ponto_de_corte] + pai[ponto_de_corte:]
-    
-    return filho1, filho2
-
-def mutacao_cb(individuo):
-    '''Realiza a mutação de um gene no problema das caixas binárias.
-    
-    Args:
-        individuo: uma lista representando um individuo no problema das caixas binárias.
-    
-    Return:
-        Um individuo com um gene mutado.
-    '''
-    
-    gene_a_ser_mutado = random.randint(0, len(individuo) - 1)
-    individuo[gene_a_ser_mutado] = gene_cb()
-    return individuo
-    
+    populacao = []
+    for n in range(tamanho):
+        populacao.append(individuo_senha(tamanho_senha, caracteres))
+    return populacao
 
 def funcao_objetivo_cb(individuo):
     '''Computa a função objetivo no problema das caixas binárias.
@@ -208,3 +169,182 @@ def funcao_objetivo_pop_cb(populacao):
         fobj = funcao_objetivo_cb(individuo)
         fitness.append(fobj)
     return fitness
+
+def funcao_objetivo_cnb(individuo):
+    '''Calcula o fitness do indivíduo para o problema das caixas não-binárias
+    
+    Args:
+        individuo: lista que representa um indivíduo dentro do problema das CNB
+        
+    Returns:
+        Um valor que representa o fitness do indivíduo
+    '''
+    
+    fitness = sum(individuo)
+    return fitness
+
+def funcao_objetivo_pop_cnb(populacao):
+    '''Calcula o fitness da população completa
+    
+    Args:
+        populacao: lista com todos os indivíduos da população
+        
+    Returns:
+        Uma lista com o fitness de cada indivíduo em ordem
+    '''
+    
+    fitness_pop = []
+    for individuo in populacao:
+        fitness_ind = funcao_objetivo_cnb(individuo)
+        fitness_pop.append(fitness_ind)
+    return fitness_pop
+
+def funcao_objetivo_senha(individuo, senha_verdadeira):
+    '''Computa a função objetivo de um indivíduo no problema da senha
+    
+    Args:
+      individuo: lista contendo os caracteres da senha
+      senha_verdadeira: a senha que o algoritmo está tentando descobrir
+      
+    Returns:
+      A diferença entre a senha proposta e a senha verdadeira (essa diferença é medida caracter por caracter, quanto mais distante um caracter estiver do caracter verdadeiro, maior é essa diferença.
+    '''
+    diferenca = 0
+
+# A função ord transforma cada caracter do computador em um numérico que é específico para cada um
+    for caracter_candidato, caracter_oficial in zip(individuo, senha_verdadeira):
+        diferenca = diferenca + abs(ord(caracter_candidato) - ord(caracter_oficial))
+
+    return diferenca
+
+
+def funcao_objetivo_pop_senha(populacao, senha_verdadeira):
+    '''Computa a função objetivo de uma população no problema da senha
+    
+    Args:
+        populacao: lista com todos os indivíduos da população
+        senha_verdadeira: a senha que você está tentando descobrir
+        
+    Returns:
+        Lista contendo os valores da métrica de distância entre as senhas
+    '''
+    
+    resultado = []
+    
+    for individuo in populacao:
+        resultado.append(funcao_objetivo_senha(individuo, senha_verdadeira))
+        
+    return resultado
+
+def mutacao_cb(individuo):
+    '''Realiza a mutação de um gene no problema das caixas binárias.
+    
+    Args:
+        individuo: uma lista representando um individuo no problema das caixas binárias.
+    
+    Return:
+        Um individuo com um gene mutado.
+    '''
+    
+    gene_a_ser_mutado = random.randint(0, len(individuo) - 1)
+    individuo[gene_a_ser_mutado] = gene_cb()
+    return individuo
+
+def mutacao_cnb(individuo, valor_max_caixa):
+    '''Realiza a mutação do indivíduo
+    
+    Args:
+        individuo: indivíduo que deve sofrer a mutação
+        valor_max_caixa: valor máximo que a caixa pode assumir
+        
+    Returns:
+        Indivíduo que sofreu a mutação
+    '''
+    gene_a_ser_mutado = random.randint(0, len(individuo)-1)
+    individuo[gene_a_ser_mutado] = gene_cnb(valor_max_caixa)
+    return individuo
+
+def mutacao_senha(individuo, caracteres):
+    '''Realiza a mutação de um gene no problema da senha
+    
+    Args:
+        individuo: uma lista representando um indivíduo no problema da senha
+        caracteres: caracteres possíveis de serem sorteados
+        
+    Return:
+        Um indivíduo (senha) com o gene modificado
+    '''
+    
+    gene = random.randint(0, len(individuo) -1)
+    individuo[gene] = gene_caracter(caracteres)
+    return individuo
+
+def selecao_roleta_max(populacao, fitness):
+    '''Seleciona individuos de uma população usando o método da roleta.
+    
+    Nota: apenas funciona para problemas de maximização.
+    
+    Arg:
+        população = lista com todos os indivíduos da população
+        fitness = valor da função objetivo dos indivíduos da população
+        
+    Return:
+        População dos indivíduos selecionados.
+    '''
+    
+    populacao_selecionada = random.choices(populacao, weights=fitness, k=len(populacao))
+    return populacao_selecionada
+
+def selecao_torneio_min(populacao, fitness, tamanho_torneio=3):
+    '''Faz a seleção de uma população usando torneio
+    
+    Nota: apenas funciona para problemas de minimização
+    
+    Args:
+        populacao: população do problema
+        fun_objetivo: função objetivo
+        tamanho_torneio: quantidade de indivíduos que batalham entre si
+    
+    Returns:
+        Lista com os indivíduos selecionados com o mesmo tamanho da população
+    '''
+    
+    selecionados = []
+    
+    # Variável criada para associar cada individuo com seu valor de fitness
+    par_populacao_fitness = list(zip(populacao, fitness))
+    
+    for _ in range(len(populacao)):
+        combatentes = random.sample(par_populacao_fitness, tamanho_torneio)
+        
+        minimo_fitness = float('inf')
+        
+        for par_individuo_fitness in combatentes:
+            individuo = par_individuo_fitness[0]
+            fit = par_individuo_fitness[1]
+            
+            if fit < minimo_fitness:
+                selecionado = individuo
+                minimo_fitness = fit
+        
+        selecionados.append(selecionado)
+        
+    return selecionados
+
+def cruzamento_ponto_simples(pai, mae):
+    '''Operador de cruzamento de ponto simples.
+    
+    Args:
+        pai: uma lista representando um indivíduo
+        mãe: uma lista representando um indivíduo
+    
+    Return:
+        Duas listas sendo que cada uma representa um filho dos pais que foram os argumentos.
+    '''
+    
+    ponto_de_corte = random.randint(1, len(pai) - 1)
+    
+    filho1 = pai[:ponto_de_corte] + mae[ponto_de_corte:]
+    filho2 = mae[:ponto_de_corte] + pai[ponto_de_corte:]
+    
+    return filho1, filho2
