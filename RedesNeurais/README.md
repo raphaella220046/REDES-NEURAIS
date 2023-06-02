f89c7032eef90dcde014c11240205a1f1782122b
Raphaella Tamyres Siqueira Gonçalves <raphaella220046@ilum.cnpem.br>
2023-06-01 22:05:32 -0300

    Inclusão dos experimentos R.05, R.06, R.07 e R.08 na pasta RedesNeurais, e atualização dos arquivos README.md e classes.py

--- RedesNeurais/README.md
+++ RedesNeurais/README.md
@@ -15,18 +15,34 @@ As redes neurais artificiais constituem uma estrutura de processamento capaz de 
 ### Experimentos:
 
 
-**O Experimento R.01 - derivadas**
+**O experimento R.01 - derivadas**
 
 Apresenta uma breve demonstração de como calcular a derivada de uma função e representá-la em um gráfico.
 
-**O Experimento R.02 - classes**
+**O experimento R.02 - classes**
 
 Apresenta uma breve introdução a estruturação de classes e métodos denominados dunders que executam ações específicas dentro da classe criada. Também demonstra como alterar o estado de um objeto dentro e fora da classe.
 
-**O Experimento R.03 - construindo um grafo automaticamente**
+**O experimento R.03 - construindo um grafo automaticamente**
 
 Apresenta a construção automática de grafos, por meio de classes, criando propriedades para armazenar os valores dos dados, operadores que executarão ações sobre esses dados e rótulos para a identificação de cada elemento no grafo.
 
-**O Experimento R.04 - computando gradientes locais**
+**O experimento R.04 - computando gradientes locais**
 
 Apresenta a primeira aplicação do gradiente local em cada vértice da rede neural por backpropagation. Fazendo uma otimização para que a propagação ocorra de maneira automática, sabendo que a rede não é cíclica, ou seja, tem início em vértices raízes e fim em um vértice folha. Desta forma, a propagação é realizada aplicando o algoritmo autograd, tornando conhecido o gradiente local de cada vértice em que se propagar, por meio de uma ordenação topológica.
+
+**O experimento R.05 - finalizando a classe Valor**
+
+Este experimento apresenta a classe `Valor` com as operações matemáticas de adição, subtração, multiplicação, divisão, exponenciação, as funções exponencial e sigmoidal, com o objetivo de entender o funcionamento de uma classe.
+
+**O experimento R.06 - redes neurais artificiais**
+
+A classe Valor desenvolvida no experimento anterior é aplicada nesse experimento, a fim de construir uma rede neural. A rede neural foi construída criando um neurônio artificial, em seguida uma camada de neurônios e, por último multiplas camadas que integram a rede neural.
+
+**O experimento R.07 - treinando uma rede neural**
+
+O objetivo desse experimento é minimizar a função de perda, utilizando pesos e vises para manipular o valor de saída da rede neural. Esta não é uma rede neural eficiente e foi aplicada apenas para demonstração de seu funcionamento, entendo o processo de calcular o erro quadrático, propagar para toda a rede, e atualizar os parâmetros, conforme a taxa de aprendizado aplicada a cada época.
+
+**O experimento R.08 - treinando uma rede neural com pytorch**
+
+Nesse experimento uma rede neural de maior complexidade é treinada pelo Pytorch, uma biblioteca Python de aprendizado de máquina desenvolvida para implementação de aprendizado profundo. Foram aplicados métodos para normalização dos dados, conversão para float32, a função retificadora entre outros. Os dados foram separados em etapas de treino e teste.
--- RedesNeurais/classes.py
+++ RedesNeurais/classes.py
@@ -1,1 +1,115 @@
- 
+import math
+
+class Valor:
+    def __init__(self, data, progenitor=(), operador_mae="", rotulo=""):
+        self.data = data
+        self.progenitor = progenitor
+        self.operador_mae = operador_mae
+        self.rotulo = rotulo
+        self.grad = 0
+
+    def __repr__(self):
+        return f"Valor(data={self.data})"
+
+    def __add__(self, outro_valor):
+        if not isinstance(outro_valor, Valor): # Chega se é uma instância
+            outro_valor = Valor(outro_valor)
+        data = self.data + outro_valor.data
+        progenitor = (self, outro_valor)
+        operador_mae = "+"
+        saida = Valor(data, progenitor, operador_mae)
+
+        def propagar_adicao():
+            self.grad += saida.grad * 1
+            outro_valor.grad += saida.grad * 1
+
+        saida.propagar = propagar_adicao
+
+        return saida
+    
+    def __radd__(self, outro_valor): # Adição de outro_valor + self, faz a troca caso a soma não funcione
+        return self + outro_valor
+
+    def __mul__(self, outro_valor):
+        if not isinstance(outro_valor, Valor): # Chega se é uma instância é um valor
+            outro_valor = Valor(outro_valor)
+        data = self.data * outro_valor.data
+        progenitor = (self, outro_valor)
+        operador_mae = "*"
+        saida = Valor(data, progenitor, operador_mae)
+
+        def propagar_multiplicacao():
+            self.grad += saida.grad * outro_valor.data
+            outro_valor.grad += saida.grad * self.data
+
+        saida.propagar = propagar_multiplicacao
+
+        return saida
+    
+    def __rmul__(self, outro_valor): # Multiplicação de outro_valor * self, faz a troca caso a operação não funcione
+        return self * outro_valor
+    
+    def __pow__(self, expoente): # self ** expoente
+        
+        assert isinstance(expoente, (int, float)) # Assegura que a condição é verdadeira
+        
+        data = self.data ** expoente
+        progenitor = (self,)
+        operador_mae = f'**{expoente}'
+        saida = Valor(data, progenitor, operador_mae)
+
+        def propagar_exponenciacao():
+            self.grad += saida.grad * expoente * (self.data ** (expoente - 1))
+
+        saida.propagar = propagar_exponenciacao
+
+        return saida
+    
+    def __truediv__(self, outro_valor): # self / outro_valor
+        return self * outro_valor ** (-1)
+        
+    def __neg__(self): # - self
+        return self * (-1)
+    
+    def __sub__(self, outro_valor): # self - outro_valor
+        return self + (-outro_valor)
+    
+    def __rsub__(self, outro_valor): # outro_valor - self
+        return self * (-1) + outro_valor
+      
+    def exp(self): # Não precisa de argumento, é a exponencial do número fornecido
+        data = math.exp(self.data)
+        progenitor = (self,) # o uso da vírgula é para o Python interprete como uma tupla e não como prioridade matemática
+        operador_mae = 'exp'
+        saida = Valor(data, progenitor, operador_mae)
+
+        def propagar_exp():
+            self.grad += saida.grad * data # A derivada da exponencial é ela mesma
+
+        saida.propagar = propagar_exp
+
+        return saida
+    
+    def sig(self):
+        return self.exp() / (self.exp() + 1)
+
+    def propagar(self):
+        pass
+
+    def propagar_tudo(self):
+        ordem_topologica = []
+        visitados = set()
+
+        def constroi_ordem_topologica(v):
+            if v not in visitados:
+                visitados.add(v)
+                for progenitor in v.progenitor:
+                    constroi_ordem_topologica(progenitor)
+                ordem_topologica.append(v)
+
+        constroi_ordem_topologica(self)
+
+        self.grad = 1  # o gradiente do vértice folha deve ser 1
+
+        for v in reversed(ordem_topologica):
+            v.propagar() 
